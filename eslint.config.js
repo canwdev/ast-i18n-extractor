44c9799ab077dfcfc98c70302a8b58e6c91ca9ee
canwdev <9585126+canwdev@users.noreply.github.com>
2026-02-05 16:48:04 +0800

    build: 更新 ESLint 配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  ignores: [
+    'test',
+  ],
+  rules: {
+    'ts/no-explicit-any': 'error',
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'regexp/no-unused-capturing-group': 'off',
+    'unused-imports/no-unused-vars': 'warn',
+  },
+})
